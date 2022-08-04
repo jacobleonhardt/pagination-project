a95a3fe426e6b934f3c754715433c8b9880404d1
Jacob Leonhardt <jacob.leonhardt649@gmail.com>
2022-08-04 17:05:27 -0500

    install react-paginate

--- react-pagination/src/components/breedpics/BreedPics.js
+++ react-pagination/src/components/breedpics/BreedPics.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const BreedPics = ({data}) => {
   return (
     <div className="row">
-      {data.map(pic => <img key={data.indexOf(pic)} src={pic} className="col col-md-4"/>)}
+      {data.map(pic => <img key={data.indexOf(pic)} src={pic} className="mh-25 p-2 col col-md-4"/>)}
     </div>
   )
 }
