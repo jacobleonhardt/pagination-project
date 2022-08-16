898beb41b861a184dd3b8b2e47c1c1effcbc5a26
Jacob Leonhardt <jacob.leonhardt649@gmail.com>
2022-08-15 23:02:50 -0500

    add style for pagination

--- react-pagination/src/components/Pagination.js
+++ react-pagination/src/components/Pagination.js
@@ -13,19 +13,19 @@ export const Pagination = ({ totalCountOfItemsInData, pageSize, siblingCount, cu
   return (
     <ul className="pagination justify-content-center">
       <li className="page-item">
-        <button disabled={currentPage === 1} onClick={() => prevClick(currentPage)}>
+        <button className={currentPage === 1 ? "page-link disabled" : "page-link"} disabled={currentPage === 1} onClick={() => prevClick(currentPage)}>
           <ion-icon name="arrow-dropleft"></ion-icon>
         </button>
       </li>
       {paginationRange.map( (page, i) => {
         if (page === DOTS) return <li key={i} className="page-item"><span className="page-link">{DOTS}</span></li>
 
-        return <li key={i} className="page-item" selected={page === currentPage} onClick={() => onPageChange(page)}>
+        return <li key={i} className={page === currentPage ? "page-item active" : "page-item"} selected={page === currentPage} onClick={() => onPageChange(page)}>
                 <span className="page-link">{page}</span>
               </li>
       })}
       <li className="page-item">
-        <button disabled={currentPage === lastPage} onClick={() => nextClick(currentPage)}>
+        <button className={currentPage === lastPage ? "page-link disabled" : "page-link"} disabled={currentPage === lastPage} onClick={() => nextClick(currentPage)}>
           <ion-icon name="arrow-dropright"></ion-icon>
         </button>
       </li>
